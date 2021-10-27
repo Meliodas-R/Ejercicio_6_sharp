42f0e76b31eac05fc6c88415400d3fef1feea4f3
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-27 14:08:38 +0200

    Agregada la operación para calcular el porcentaje del número introducido y el muestreo por pantalla.

---
+++ Ejercicio_6_sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio_6_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal numero;
+
+            Console.Write("Introduzca un número: ");
+            numero = Convert.ToDecimal(Console.ReadLine());
+
+            numero = (133 * numero) / 100;
+
+            Console.WriteLine("El 133% del número introducido es: " + numero);
+        }
+    }
+}
